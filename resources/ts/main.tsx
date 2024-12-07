cd3f3c906300e9ff095c89d3737c41ee8180a76b
Borsos Szilárd <borsosszilard@csihazrt.hu>
2024-12-07 16:30:56 +0100

    Removed inertia, created default react app

---
+++ resources/ts/main.tsx
@@ -0,0 +1,13 @@
+import {StrictMode} from "react";
+import {App} from "./app";
+import {createRoot} from "react-dom/client";
+
+const root = createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <StrictMode>
+            <App />
+    </StrictMode>
+);
