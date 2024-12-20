bc7e39c09c974797bce836444e0bff55c0c86a83
Borsos Szilárd <borsosszilard@csihazrt.hu>
2024-12-20 12:52:23 +0100

    #2 Create login page -Feature: implemented basic login -Feature: implemented google oauth provider

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,2 +1,12 @@
+import {Routes, Route} from "react-router";
+import {routes} from "./routes";
+
 export const App = () => {
+    return (
+        <div>
+            <Routes>
+                {routes?.map((route) => <Route {...route} key={route.path}/> )}
+            </Routes>
+        </div>
+    );
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,15 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Scripts -->
         @viteReactRefresh
-        @vite(['resources/ts/main.tsx'])
+        @vite(['resources/ts/main.tsx','resources/css/app.css'])
     </head>
     <body>
         <div id="root"></div>
